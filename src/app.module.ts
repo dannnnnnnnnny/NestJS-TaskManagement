ea1cb1c0a7aae8074a6d8525de8c9bc3481e78ec
ehdgnl5249 <ehdgnl5249@naver.com>
2021-04-04 18:22:58 +0900

    ADD: websockets tutorial

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { LoggerModule } from 'nestjs-pino';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule, LoggerModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule, LoggerModule.forRoot(), ChatModule],
 })
 export class AppModule {}
